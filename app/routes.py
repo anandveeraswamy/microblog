aeb77fd47b04daaf6b9fbda05c755e5dffa86711
anandadagit <anandveeraswamy@gmail.com>
2024-02-10 10:15:59 +0000

    Added index template

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>'''
+    return render_template('index.html', title='Home', user=user)
